07c207a5e436f587453225686df7084ab24e6f29
Sorin Lupastean <lupasteansorin1@gmail.com>
2024-08-28 15:26:26 +0300

    Add user schema

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
-mongoose.connect("mongodb://localhost:27017/databases");
+mongoose.connect("mongodb://localhost:27017/local");
 
 app.listen(3001, () => {
   console.log("Server is running");
---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    trim: true,
+    unicque: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    requiere: true,
+    minlength: 6,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
